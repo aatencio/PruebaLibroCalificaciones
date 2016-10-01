7c5518e66bae57ed338990406c3e57823a0ebee0
Arnaldo Atencio <arnaldo.atencio@gmail.com>
2016-10-01 13:50:25 -0500

    INICIO

---
+++ src/LibroCalificaciones.java
@@ -0,0 +1,12 @@
+/*Actividad del libro, ver pagina 73
+Fig. 3.1 */
+
+public class LibroCalificaciones 
+{
+	//Muestra un mensaje de bienvenida al usuario
+	public void mostrarMensaje()
+	{
+		System.out.println("Bienvenido al libro de calificaciones");
+	}// fin del metodo mostrar mensaje
+
+}//fin de la clase LibroCalificaciones
---
+++ src/PruebaLibroCalificaciones.java
@@ -0,0 +1,18 @@
+/*Clase main para el ejercicio del libro, pag 74 Fig. 3.2*/
+// Crea un objeto libroCalificaciones y llama a su metodo
+
+public class PruebaLibroCalificaciones {
+	//Aqui inicia la ejecucion del programa
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Crea un objeto libroCalificaciones y lo asigna a
+		//miLibroCalificaciones
+		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+		
+		//ahora llama al metodo del objeto creado
+		miLibroCalificaciones.mostrarMensaje();
+		
+	}// fin de la clase main
+
+}// fin de la clase PruebaLibroCalificaciones
