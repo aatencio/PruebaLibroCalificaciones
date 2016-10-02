a8345baf88af4f021abbb2a073948a560ce8da8a
Arnaldo Atencio <arnaldo.atencio@gmail.com>
2016-10-02 15:09:33 -0500

    Entrada nombre del curso

--- src/LibroCalificaciones.java
+++ src/LibroCalificaciones.java
@@ -4,9 +4,10 @@ Fig. 3.1 */
 public class LibroCalificaciones 
 {
 	//Muestra un mensaje de bienvenida al usuario
-	public void mostrarMensaje()
+	public void mostrarMensaje(String nombreDelCurso)
 	{
-		System.out.println("Bienvenido al libro de calificaciones");
+		System.out.printf("Bienvenido al libro de calificaciones para \n%sHola\n", 
+							nombreDelCurso );
 	}// fin del metodo mostrar mensaje
 
 }//fin de la clase LibroCalificaciones
--- src/PruebaLibroCalificaciones.java
+++ src/PruebaLibroCalificaciones.java
@@ -1,17 +1,27 @@
 /*Clase main para el ejercicio del libro, pag 74 Fig. 3.2*/
 // Crea un objeto libroCalificaciones y llama a su metodo
 
+import java.util.Scanner; //usa la clase Scanner
+
 public class PruebaLibroCalificaciones {
 	//Aqui inicia la ejecucion del programa
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Crea el objeto Scanner para leer los datos de la ventana de comandos
+		Scanner entrada = new Scanner(System.in);
+		
 		//Crea un objeto libroCalificaciones y lo asigna a
 		//miLibroCalificaciones
 		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
 		
+		//pide y recibe el nombre del curso como entrada
+		System.out.println("Escriba el nombre del curso");
+		String nombreDelCurso = entrada.nextLine();//lee una linea de texto
+		
+		
 		//ahora llama al metodo del objeto creado
-		miLibroCalificaciones.mostrarMensaje();
+		miLibroCalificaciones.mostrarMensaje(nombreDelCurso);
 		
 	}// fin de la clase main
 
